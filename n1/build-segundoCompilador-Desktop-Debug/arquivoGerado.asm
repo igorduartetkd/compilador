201c7f3bc537092ea5f756fae6600bcdac9c1a7a
Igor Duarte <igor@Jhonny>
2018-04-26 20:46:23 -0300

    inicio analisador ILOC

--- n1/build-segundoCompilador-Desktop-Debug/arquivoGerado.asm
+++
@@ -1,6 +0,0 @@
-	global main			;indica que o main sera visivel fora do arquivo
-	extern printf, scanf			;funções escritas em c que serão usadas no programa
-
-	section .data			;segmento de dados inicializados
-
-	ret				;final do programa
